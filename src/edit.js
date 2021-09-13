9115f7aff5fbc8d997b3f3e0ef5291555783bde7
Tim-Fang <mc13735292967@163.com>
2021-09-13 17:04:02 +0800

    add config

--- src/edit.js
+++ src/edit.js
@@ -12,8 +12,9 @@ function getQueryVariable(variable) {
 var data = decodeURIComponent(getQueryVariable("data"));
 var onEdit_week = getQueryVariable("onEditWeek")
 var onEdit_num = Number(getQueryVariable("onEditNum"))
-data=JSON.parse(data).courses[onEdit_week][onEdit_num]
-window.opener.postMessage(data);
+console.log(onEdit_week, onEdit_num);
+data = JSON.parse(data).courses[onEdit_week][onEdit_num]
+
 
 $("body > div > div:nth-child(2) > input")[0].value = data.name;
 $("body > div > div:nth-child(4) > input:nth-child(1)")[0].value = data.time_start.split(":")[0];
@@ -21,3 +22,16 @@ $("body > div > div:nth-child(4) > input:nth-child(3)")[0].value = data.time_sta
 $("body > div > div:nth-child(6) > input:nth-child(1)")[0].value = data.time_end.split(":")[0];
 $("body > div > div:nth-child(6) > input:nth-child(3)")[0].value = data.time_end.split(":")[1];
 
+$("#save_temp").on("click", function () {
+
+});
+$("#save_forever").on("click", function () {
+    data.name = $("body > div > div:nth-child(2) > input")[0].value;
+    data.time_start = $("body > div > div:nth-child(4) > input:nth-child(1)")[0].value + ":" + $("body > div > div:nth-child(4) > input:nth-child(3)")[0].value;
+    data.time_end = $("body > div > div:nth-child(6) > input:nth-child(1)")[0].value + ":" + $("body > div > div:nth-child(6) > input:nth-child(3)")[0].value;
+    window.opener.postMessage({ action: "edit_saveforever", data: { week: onEdit_week, num: onEdit_num, data: data } });
+    window.close()
+});
+$("#save_cancel").on("click", function () {
+    window.close()
+});
