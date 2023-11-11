2cc0bb6b2ca2d54b67d36df68b2fff3790c9520d
TimFang <mc13735292967@163.com>
2023-11-11 12:34:37 +0800

    [feat] 窗口背景模糊
    Signed-off-by: TimFang <mc13735292967@163.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod setup;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+   format!("Hello, {}!", name)
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greet])
+    .setup(setup::init)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import { NButton } from 'naive-ui'
+import { invoke } from '@tauri-apps/api'
+
+// now we can call our Command!
+// Right-click the application background and open the developer tools.
+// You will see "Hello, World!" printed in the console!
+invoke('greet', { name: 'World' })
+  // `invoke` returns a Promise
+  .then((response) => console.log(response))
 </script>
 
 <template>
@@ -11,6 +20,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+    <n-button strong secondary type="primary">naive-ui</n-button>
+
   <HelloWorld msg="Vite + Vue" />
 </template>
 
@@ -21,9 +32,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
